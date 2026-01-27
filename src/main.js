47b97668d485fa2a91023aae13d1708671d4234f
Po Lung Wu <wupojung@gmail.com>
2026-01-28 01:41:50 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,2 @@
+import './assets/style.css';
+console.log('專案啟動成功！');
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+
+export default defineConfig({
+    plugins: [
+        injectHTML({
+            tagName: 'include', // 將原本的 load 改為 include
+        }), // 啟用 HTML 分段功能
+    ],
+    server: {
+        port: 3000, // 或是你喜歡的 port
+        open: true  // 自動開啟瀏覽器
+    }
+});
