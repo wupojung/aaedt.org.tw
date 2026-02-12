dff738c0521a1ac6cfbd169d6e2c5730b683a19e
Po Lung Wu <wupojung@gmail.com>
2026-02-13 01:58:36 +0800

    重新調整about 頁面當範本

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import './assets/style.css';
 import './assets/styleguide.css';
 import './assets/accessibility.css';
 import './assets/about.css';
-import './assets/organization.css';
-
+//import './assets/organization.css';
+import './assets/breadcrumb.css';   //breadcrumb nav
 
 
 import './scripts/aaedt.js';
